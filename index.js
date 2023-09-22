ddd75a87fb7347421e7f25dacc7d6cabf2d40860
rakib4485 <mdrakibulislam4485@gmail.com>
2023-09-22 23:11:23 +0600

    user upload code is written

--- index.js
+++ index.js
@@ -72,6 +72,12 @@ async function run() {
         option
       );
       res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     })
    
   }
