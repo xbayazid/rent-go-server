2dcd5b771d9f96d71ee11e94517e261053788c49
rakib4485 <mdrakibulislam4485@gmail.com>
2023-11-19 23:21:02 +0600

    initital deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion,ObjectId  } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -25,18 +26,89 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
     }
   });
+
+  function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+      return res.status(401).send('unauthorized access')
+    }
+  
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+      if(err){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
+
 async function run() {
   try {
     await client.connect(); 
-    // const blogsCollection = client.db("psychologyBuzz").collection("blogs");
     const housesCollection = client.db("rentgo").collection("houses");
     const usersCollection = client.db("rentgo").collection("users");
+    const bookingsCollection = client.db("rentgo").collection("bookings");
+    const transportBookingCollection = client.db("rentgo").collection("transportBooking");
+    const wishListCollection = client.db("rentgo").collection("wishlist");
+
+    app.get('/jwt', async(req, res) => {
+      const email = req.query.email;
+      const query = {
+        email: email
+      };
+
+      const user = await usersCollection.findOne(query);
+      if(user) {
+        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {
+          expiresIn: "24h",
+        })
+        return res.send({accessToken: token})
+      }
+      res.status(403).send({accessToken: ""})
+    });
+
+    // Note: Make sure you use verifyAdmin after verifyJWT
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
 
     app.get('/properties', async (req, res) => {
+        const search = req.query;
+        const city = await req.query.city;
+        const area =await req.query.area;
+        const type =await req.query.type;
+        if(city){
+          const query = {
+            city : city,
+            area: area,
+            propertyType: type
+          }
+          const houses = await housesCollection.find(query).toArray();
+          return res.send(houses)
+        }
         const query = {};
         const houses = await housesCollection.find(query).toArray();
         res.send(houses);
     });
+
+    app.post("/properties",verifyJWT, async (req, res) => {
+      const property = req.body;
+      const result = await housesCollection.insertOne(property);
+      res.send(result);
+    })
+
+    app.get("/searchProperty", async (req, res) => {
+      const query = req.query;
+      console.log(query);
+    })
 
     app.get('/propertyDetails/:id', async (req, res) => {
         const id = req.params.id;
@@ -74,11 +146,155 @@ async function run() {
       res.send(result);
     });
 
+    // booking related code
+    app.post("/bookings", async (req, res) => {
+      console.log("booking api is called");
+      const booking = req.body;
+      const query = {
+        email: booking.email,
+        propertyId: booking.propertyId
+      };
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
+      if (alreadyBooked.length) {
+        const message = `You already have a booking on this property`;
+        return res.send({ acknowledged: false, message });
+      }
+      const result = await bookingsCollection.insertOne(booking);
+      res.send({acknowledged: true});
+    });
+
+    app.get("/bookings",verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+
+      if(email !== decodedEmail){
+        return res.status(403).send("forbidden access");
+      }
+      const query = {email: email};
+      const booking = await bookingsCollection.find(query).toArray();
+      res.send(booking)
+    });
+
+    app.get("/allBookings", async (req, res) => {
+      const query = {};
+      const booking = await bookingsCollection.find(query).toArray();
+      res.send(booking)
+    });
+
+    app.get("/transportBooking",verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+
+      if(email !== decodedEmail){
+        return res.status(403).send("forbidden access");
+      }
+      const query = {userEmail: email};
+      const booking = await transportBookingCollection.find(query).toArray();
+      res.send(booking)
+    });
+
+    app.get("/allTransportBookings", async (req, res) => {
+      const query = {};
+      const booking = await transportBookingCollection.find(query).toArray();
+      res.send(booking);
+    })
+
+    app.post("/transportBooking", async(req, res) =>{
+      const booking = req.body;
+     const query = {
+        userEmail: booking.userEmail
+      };
+      const alreadyBooked = await transportBookingCollection.find(query).toArray();
+      if (alreadyBooked.length) {
+        const message = `You already have a booking on this property`;
+        return res.send({ acknowledged: false, message });
+      }
+      const result = await transportBookingCollection.insertOne(booking);
+      res.send({acknowledged: true}); 
+    })
+
+    // USER CODE
+    
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const user = await usersCollection.find(query).toArray();
+      res.send(user);
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = {
+        email: user.email
+      };
+      const alreadyBooked = await usersCollection.find(query).toArray();
+      if (alreadyBooked.length) {
+        const message = `You already have a booking on this property`;
+        return res.send({ acknowledged: false, message });
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+    app.get("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+
+    app.get("/user/owner/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isOwner: user?.role === "owner" });
+    });
+    // WishList
+
+    app.get('/wishlist', async(req, res) => {
+      const email = req.query.email;
+      const properties = await housesCollection.find({}).toArray();
+      let wishlist = [];
+      const wishlistQuery = {email: email};
+      const mylist = await wishListCollection.find(wishlistQuery).toArray();
+
+      properties.forEach(async (property) =>{
+        mylist.forEach(li => {
+          if(JSON.stringify(property._id) === JSON.stringify(li.propertyId)){
+            wishlist = [...wishlist, property]
+          }
+        })
+      })
+      res.send(wishlist)
     })
+
+    app.post("/wishlist", async(req,res) => {
+      const list = req.body;
+      const query = {
+        propertyId: list.propertyId
+      };
+      const alreadyBooked = await wishListCollection.find(query).toArray();
+      if (alreadyBooked.length) {
+        const message = `Already have this property in your wishlist`;
+        return res.send({ acknowledged: false, message });
+      }
+      const result = wishListCollection.insertOne(list);
+      res.send({acknowledged: true});
+    })
+
+    // temporary to update a field
+    // app.get('/addIsRent', async(req, res) => {
+    //     const filter = {};
+    //     const option = { upsert : true };
+    //     const updatedDoc = {
+    //         $set: {
+    //          propertyImage: 'https://i.ibb.co/sPJChqL/property-header-1.jpg'
+    //         }
+    //     }
+    //     const result = await bookingsCollection.updateMany(filter, updatedDoc, option);
+    //     res.send(result);
+    // })
    
   }
   finally{
